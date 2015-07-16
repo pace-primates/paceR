3da1f02e29a70fe1d697db09bd9e2a6fe09bc858
Fernando Campos <camposfa@gmail.com>
2015-07-16 16:16:12 -0600

    Added micro-benchmarking for get_pace_tbl()

--- scripts/package_testing.R
+++ scripts/package_testing.R
@@ -1,7 +1,7 @@
 Sys.setenv(TZ = 'UTC')
-list.of.packages <- list("devtools", "roxygen2")
+list.of.packages <- list("devtools", "roxygen2", "microbenchmark")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(unlist(new.packages))
+if (length(new.packages)) install.packages(unlist(new.packages))
 lapply(list.of.packages, require, character.only = T)
 
 # Install the latest version of the package
@@ -15,6 +15,9 @@ library(paceR)
 load_pace_packages()
 
 # Create the SSH tunnel (modify for your connection)
+# If corrupt, in terminal:
+# ps -ax | grep ssh
+# kill <pid>
 system('ssh -f camposf@pacelab.ucalgary.ca -L 3307:localhost:3306 -N')
 pace_db <- src_mysql(group = "PACE", user = "camposf", dbname = "monkey", password = NULL)
 
@@ -29,3 +32,20 @@ ind <- get_individuals(pace_db, full = FALSE)
 deaths <- get_pace_tbl(pace_db, "tblIndividualDeath")
 
 bio <- get_biography(pace_db)
+
+get_infanticide_risk(pace_db)
+
+
+
+# Benchmarking
+microbenchmark(
+  v_ind <- get_pace_tbl(pace_db, "vwIndividual") %>% filter(Project == "SR"),
+  v_ind <- get_pace_tbl(pace_db, "vwIndividual", collect = FALSE) %>% filter(Project == "SR") %>% collect(),
+  times = 20
+)
+
+# Does chaining to a view require executing the entire view?
+get_pace_tbl(pace_db, "vwIndividual", collect = FALSE) %>%
+  filter(Project == "SR") %>%
+  explain()
+# No, it simply adds to the query
