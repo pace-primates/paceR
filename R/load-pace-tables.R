655f070fa590429163eaeaa743bd2a58661de55c
Fernando Campos <camposfa@gmail.com>
2015-07-16 16:12:31 -0600

    Added option to collect table or not

--- R/load-pace-tables.R
+++ R/load-pace-tables.R
@@ -3,24 +3,29 @@
 #'
 #' @param pace_db The src_mysql connection to the PACE Database.
 #' @param tbl_name Name of the table, view, or query you want to get.
+#' @param collect Option to collect the data or not. Default is TRUE.
 #'
 #' @export
 #' @examples
 #' get_pace_tbl(pace_db, "tblGroup")
 
-get_pace_tbl <- function(pace_db, tbl_name){
+get_pace_tbl <- function(pace_db, tbl_name, collect = TRUE){
 
-  get_pace_tbl_internal <- function(pace_db, tbl_name){
+  get_pace_tbl_internal <- function(pace_db, tbl_name, collect){
 
     t1 <- pace_db %>%
       tbl(tbl_name) %>%
-      select(-starts_with("TimeStamp"), -starts_with("UserAdd")) %>%
-      collect()
+      select(-starts_with("TimeStamp"), -starts_with("UserAdd"))
 
-    return(t1)
+    if (collect) {
+      return(collect(t1))
+    }
+    else {
+     return(t1)
+    }
   }
 
-  t2 <- suppressWarnings(get_pace_tbl_internal(pace_db, tbl_name))
+  t2 <- suppressWarnings(get_pace_tbl_internal(pace_db, tbl_name, collect))
 
   return(t2)
 
