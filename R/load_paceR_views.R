6d435d39d03e5550f0118cd7a7d979dd5677ed87
Fernando Campos <camposfa@gmail.com>
2015-09-02 13:35:02 -0600

    Added getv_Phenology

--- R/load_paceR_views.R
+++ R/load_paceR_views.R
@@ -48,3 +48,27 @@ getv_Individual <- function(paceR_db, full = TRUE){
 
   return(ind)
 }
+
+#' Get table with Phenology data
+#'
+#' @param paceR_db The src_mysql connection to the paceR Database.
+#' @param full Option to return the full table (TRUE) or just a condensed version (FALSE). Default is TRUE.
+#'
+#' @export
+#' @examples
+#' getv_Phenology(paceR_db)
+getv_Phenology <- function(paceR_db, full = TRUE){
+
+  p <- get_pace_tbl(paceR_db, "vPhenology")
+
+  p <- p %>%
+    mutate_each(funs(as.Date), contains("Date"))
+
+  if (!full) {
+    p <- p %>%
+      select(Project, PhenologyDate, TreeLabel, FoodPart, Measurement,
+             PhenologyScore, PhenologyPercent, PhenologyCount)
+  }
+
+  return(p)
+}
