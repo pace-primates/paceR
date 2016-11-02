884f613db675106053090616315a15ad923993f1
Fernando Campos <camposfa@gmail.com>
2016-11-02 09:21:10 -0500

    Another CGRA fix (in the script only)

--- scripts/pheno-analysis-script.R
+++ scripts/pheno-analysis-script.R
@@ -79,6 +79,12 @@ plot_fpv_dbh(fpv)
 # Fix minimum DBHs (currently done manually, need to verify)
 min_dbh <- fpv_get_min_dbh_sr(fpv)
 
+# Set CGRA manually
+min_dbh <- bind_rows(min_dbh,
+                     data.frame(code_name = "CGRA",
+                                threshold_dbh = 10,
+                                n_trees = 1))
+
 # Get relevant transect data corresponding to pheno species
 # Also exclude individual trees that are too small to produce food based on FPVs
 tr_pheno_fpv <- transect_subset_sr(tr, pheno, min_dbh)
